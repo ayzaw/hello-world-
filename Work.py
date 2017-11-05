ee0c605e08237e84fe6693a494e36f5ef774301a
ayzaw <32266032+ayzaw@users.noreply.github.com>
2017-11-05 16:36:40 +0000

    Add files via upload

---
+++ Work.py
@@ -0,0 +1,28 @@
+print ("Hellow World")
+print ("I am in my ISD class right now")
+
+
+name = input("Hi there, What is your name?")
+print("Hello "  +name+   " How are you?")
+
+
+
+
+num = 4
+num*=2
+num1=num+2
+num1+=3
+print(num1)
+
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+result = float (num1) + float(num2)
+
+print('The result of {0} and {1} is {2}'.format(num1, num2, result))
+
+
+
+
+                              
